abf24423ee98f65e7b09b32eed54a8183d4c279e
mindok7520 <mindok7520@users.noreply.github.com>
2026-06-01 06:55:59 +0000

    deploy: f3b313b53c1ffa01c8c20c009576afc5abde1e00

---
+++ docs/c/search/variables_b.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['value_0',['value',['../structopendal__metadata__user__metadata__pair.html#ae927319aefc7d3ead7e75b951bb54d81',1,'opendal_metadata_user_metadata_pair::value'],['../structopendal__http__header__pair.html#a671d9bea49fdd5c6b9486e5bacbd1406',1,'opendal_http_header_pair::value']]]
+];
